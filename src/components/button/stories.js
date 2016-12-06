151b3b78ed361afa0bbad3d710ded0cc2912781a
Federico Knüssel <fknussel@gmail.com>
2016-12-06 14:39:56 -0300

    Update Button stories

--- src/components/button/stories.js
+++ src/components/button/stories.js
@@ -2,22 +2,45 @@ import React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
 import Button from '.';
 
+const log = action('You\'ve just clicked the button');
+
 storiesOf('Button')
+  .add('of type primary and small size', () => (
+    <Button type="primary" size="small" onClick={log}>Save changes</Button>
+  ))
+  .add('of type primary and default size', () => (
+    <Button type="primary" onClick={log}>Save changes</Button>
+  ))
+  .add('of type primary and large size', () => (
+    <Button type="primary" size="large" onClick={log}>Save changes</Button>
+  ))
+  .add('of type confirm and small size', () => (
+    <Button type="success" size="small" onClick={log}>Export</Button>
+  ))
   .add('of type success', () => (
-    <Button title="I'm a green button" type="success" action={action('clicked')} />
+    <Button type="success" onClick={log}>Export</Button>
+  ))
+  .add('of type success and large size', () => (
+    <Button type="success" size="large" onClick={log}>Export</Button>
+  ))
+  .add('of type danger and small size', () => (
+    <Button type="danger" size="small" onClick={log}>Remove</Button>
   ))
   .add('of type danger', () => (
-    <Button title="I'm a red button" type="danger" action={action('clicked')} />
+    <Button type="danger" onClick={log}>Remove</Button>
   ))
-  .add('of type warning', () => (
-    <Button title="I'm a yellow button" type="warning" action={action('clicked')} />
+  .add('of type danger and large size', () => (
+    <Button type="danger" size="large" onClick={log}>Remove</Button>
   ))
-  .add('of type info', () => (
-    <Button title="I'm a blue button" type="info" action={action('clicked')} />
+  .add('of type default and small size', () => (
+    <Button size="small" onClick={log}>Logout</Button>
   ))
   .add('of type default', () => (
-    <Button title="I'm a gray button" action={action('clicked')} />
+    <Button onClick={log}>Logout</Button>
+  ))
+  .add('of type default and large size', () => (
+    <Button size="large" onClick={log}>Logout</Button>
   ))
   .add('cool button with emojis', () => (
-    <Button title="😀 😎 👍" type="success" action={action('clicked')} />
+    <Button type="create" onClick={log}>😀 😎 👍</Button>
   ));
