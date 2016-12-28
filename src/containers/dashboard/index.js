4b03d25a58ab60d0a9341c9141608d6da30f2606
Federico Knüssel <fknussel@users.noreply.github.com>
2016-12-28 10:44:54 -0300

    Make Dashboard a stateless component

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -6,40 +6,32 @@ import {showSpinner} from 'actions/spinner';
 import Button from 'components/button';
 import styles from './styles.css';
 
-class Dashboard extends React.Component {
-  render() {
-    return (
-      <section className={styles.wrapper}>
-        <h1>Lorem ipsum dolor sit amet</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-          qui officia deserunt mollit anim id est laborum.</p>
+const Dashboard = ({ showAlert, hideAlert, showSpinner }) => (
+  <section className={styles.wrapper}>
+    <h1>Lorem ipsum dolor sit amet</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-        <Button className={styles.button} type="success" onClick={() => this.props.showAlert('Hello world', 'success')}>
-          Trigger a success alert
-        </Button>
+    <Button className={styles.button} type="success" onClick={() => showAlert('Hello world', 'success')}>
+      Trigger a success alert
+    </Button>
 
-        <Button className={styles.button} type="danger" onClick={() => this.props.showAlert('Hello world', 'error')}>
-          Trigger an error alert
-        </Button>
+    <Button className={styles.button} type="danger" onClick={() => showAlert('Hello world', 'error')}>
+      Trigger an error alert
+    </Button>
 
-        <Button className={styles.button} type="primary" onClick={() => this.props.showAlert('Hello world', 'info')}>
-          Trigger an info alert
-        </Button>
+    <Button className={styles.button} type="primary" onClick={() => showAlert('Hello world', 'info')}>
+      Trigger an info alert
+    </Button>
 
-        <Button className={styles.button} onClick={this.props.hideAlert}>
-          Hide the alert
-        </Button>
+    <Button className={styles.button} onClick={hideAlert}>
+      Hide the alert
+    </Button>
 
-        <Button className={styles.button} onClick={this.props.showSpinner}>
-          Display the loading spinner
-        </Button>
-      </section>
-    );
-  }
-}
+    <Button className={styles.button} onClick={showSpinner}>
+      Display the loading spinner
+    </Button>
+  </section>
+);
 
 Dashboard.propTypes = {
   // Redux actions
