f245ee3388f12e5c5050341711015be1f16e674c
Federico Knüssel <fknussel@gmail.com>
2016-12-29 17:01:14 -0300

    Add Flowtype support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+// @flow
+
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import routes from 'routes';
-import store from 'store';
+import routes from './routes';
+import store from './store';
 
 // Global Styles
-import 'styles/main.css';
+import './styles/main.css';
+
+const name: string = 'blah';
+
+function sum(a: number, b:number) {
+  return a + b;
+}
+
+console.log(sum(1, 4));
 
 render(
   <Provider store={store}>
