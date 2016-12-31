603bedd2305e878e9397f545c9152d3a8cfe6c4a
Federico Knüssel <fknussel@gmail.com>
2016-12-31 00:17:39 -0300

    Add Git Hook to run tests before commiting code

--- test/button.spec.js
+++ test/button.spec.js
@@ -2,17 +2,18 @@ import React from 'react';
 import sinon from 'sinon';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
-import Button from '../src/components/button/button';
+import Button from '../src/components/button';
 
 describe('Button', () => {
   it('renders only one instance of the Button component', () => {
-    const button = shallow(<Button title="Test" type="success" />);
+    const button = shallow(<Button type="success">Test Button</Button>);
 
     expect(button).to.have.length(1);
   });
 
   it('logs out a message when clicked', () => {
-    const button = shallow(<Button title="Test" type="success" />);
+    const log = () => console.log('You have just clicked on the button');
+    const button = shallow(<Button onClick={log}>Test Button</Button>);
     const spy = sinon.spy(console, 'log');
 
     button.simulate('click');
